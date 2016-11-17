4e8e61f5d7cb71741f69f8a2f1c5964636d7ee84
tophboogie <tophboogie@gmail.com>
2016-11-17 01:42:56 -0800

    modified to use Watson API as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# nodejs
+# nodeJS
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get install -y nodejs
 
-# database installs
+# mongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
--- scrapeTwitter.js
+++ scrapeTwitter.js
@@ -1,25 +1,78 @@
-var Twit = require('twit')
+var user_id = '25073877' // Trump user id
 
-var T = new Twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  app_only_auth:        true,
-  timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
+var Twit = require('twit')
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3')
+var Promise = require('promise')
+var JsonQuery = require('json-query')
+
+var Twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  app_only_auth: true
 })
 
-T.get('statuses/user_timeline', {
-    user_id: '25073877',
-    trim_user: true,
-    exclude_replies: true,
-    include_rts: false
-  }, (error, data, response) => {
-    var newTweets = []
-    data.forEach((element, i) => {
-      newTweets[i] = {
-        text: element.text,
-        created_at: element.created_at
+var ToneAnalyzer = new ToneAnalyzerV3({
+  username: process.env.WATSON_USERNAME,
+  password: process.env.WATSON_PASSWORD,
+  version_date: '2016-05-19'
+})
+
+var TwitterSync = () => {
+  return new Promise((resolve, reject) => {
+    Twitter.get('statuses/user_timeline', {
+        user_id: user_id,
+        trim_user: true,
+        exclude_replies: true,
+        include_rts: false
+      }, (err, data, resp) => {
+        if (err)
+          reject(err)
+        else
+          resolve(processTweets(data))
       }
+    )
+  })
+}
+
+var WatsonSync = (data) => {
+  return new Promise((resolve, reject) => {
+    ToneAnalyzer.tone({text: data.text}, (err, data, resp) => {
+      if (err)
+        reject(error)
+      else
+        resolve(processTone(data))
     })
-    console.log(newTweets)
-  }
-)
+  })
+}
+
+var WatsonBulk = (data) => {
+  var promises = []
+  data.forEach((element, i) => {
+    promises.push(WatsonSync(element))
+  })
+  return Promise.all(promises)
+}
+
+function processTweets(data) {
+  new_tweets = []
+  data.forEach((element, i) => {
+    new_tweets[i] = {
+      text: element.text,
+      created_at: element.created_at,
+    }
+  })
+  return new_tweets
+}
+
+function processTone(data) {
+  return JsonQuery('tone_categories[category_id=emotion_tone].tones', {data: data.document_tone}).value
+}
+
+TwitterSync()
+  .then(WatsonBulk)
+  .then(function(data) {
+    new_tweets.forEach((element, i) => {
+      element.tone = data[i]
+    })
+    console.log(JSON.stringify(new_tweets))
+  })
