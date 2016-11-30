113307a4a14d27661b7cc1b21f94683de70fece4
tophboogie <tophboogie@gmail.com>
2016-11-29 17:16:59 -0800

    route finds tweets only within given date range
    —returns just date and text for now
    —returns 6/1 thru 6/9 arbitrarily
    —still needs http GET to include dates in url and variable in mongo call

--- backend/api.js
+++ backend/api.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
-var Tweet = require('./tweetModel.js')
+var Tweet = require('./models/tweet.js')
 var mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/tweets')
@@ -16,17 +16,20 @@ app.use(bodyParser.json())
 app.use(cors())
 
 router.route('/').get(function(req, res) {
-  Tweet.find((err, tweets) => {
-    if (err)
-      res.send(err)
-    else
-      res.json(tweets)
-  })
-})
-
-router.route('/tonify').post(function(req, res) {
-  console.log(req.body.id)
-  res.send('tone tone tone')
+  Tweet.find({
+    tweetDate: {
+      $gte: new Date(2016, 5, 1),
+      $lt: new Date(2016, 5, 10)
+    }})
+    .sort('tweetDate')
+    .select('tweetDate tweetText')
+    .exec(function (err, tweets) {
+      if (err)
+        res.send(err)
+      else {
+        res.json(tweets)
+      }
+    })
 })
 
 app.use('/api', router)
