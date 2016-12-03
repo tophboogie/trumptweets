f91e7bf27055040163c6313f141fa2f0ac9d6e9a
tophboogie <tophboogie@gmail.com>
2016-12-03 15:33:29 -0800

    working example of wordcloud display
    -d3 and react happily married
    -wordcloud populates with api call from server
    -needs cleanup
    -also better regular expressions to filter stupid shit out
    -display looks really fugly

--- frontend/dev/jsx/home.jsx
+++ frontend/dev/jsx/home.jsx
@@ -2,27 +2,59 @@ import React from 'react'
 import Axios from 'axios'
 
 import Wordcloud from './wordcloud.jsx'
+import removeStopWords from '../lib/stopwords.js'
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tweets: []
+      tweets: [],
+      words: [
+            "Hello", "world", "normally", "you", "want", "more", "words",
+            "than", "this"].map(function(d) {
+            return {text: d, size: 10 + Math.random() * 90};
+          })
     }
   }
   componentDidMount() {
     Axios.get('http://localhost:3030/tweets')
       .then((resp) => {
         this.setState({
-          tweets: resp.data
+          tweets: resp.data,
+          words: this.getWordArray(resp.data)
         })
       })
   }
+  getWordArray(tweets) {
+    var words = []
+    var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    var puncuationRegex = /[",;.!?':1234567890]/g
+    tweets.forEach((tweet) => {
+      var test = removeStopWords(tweet.tweetText.toLowerCase().trim())
+      var tempWords = test.replace(urlRegex, '').replace(puncuationRegex,'')
+      tempWords.split(/[\s\/]+/g).forEach((word) => {
+        words.push(word)
+      })
+    })
+    var wordMap = words.sort().reduce((prev, cur) => {
+      prev[cur] = (prev[cur] || 0) + 1;
+      return prev;
+    }, {});
+    var wordMap2 = []
+    for (var key in wordMap) {
+      if (wordMap.hasOwnProperty(key)) {
+        wordMap2.push({
+          text: key,
+          size: wordMap[key]
+        })
+      }
+    }
+    return wordMap2
+  }
   render() {
-    //console.log(this.state.tweets)
     return (
       <div className="container gutter-top">
-        <Wordcloud tweets={this.state.tweets} />
+        <Wordcloud words={this.state.words} />
       </div>
     )
   }
--- frontend/dev/jsx/wordcloud.jsx
+++ frontend/dev/jsx/wordcloud.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import node from './d3/test.js'
+import createWordcloud from './d3/wordcloud.js'
 import rd3 from 'react-d3-library'
 const RD3Component = rd3.Component
 
@@ -10,16 +10,14 @@ class Wordcloud extends React.Component {
       d3: ''
     }
   }
-  componentDidMount() {
+  componentWillReceiveProps(props) {
     this.setState({
-      d3: node
+      d3: createWordcloud(props.words)
     })
   }
   render() {
     return (
-      <div>
-        <RD3Component data={this.state.d3} />
-      </div>
+      <RD3Component data={this.state.d3} />
     )
   }
 }
