44314478a2e98fc09434d9027051346f34dea52b
Steven Hein <steven.c.hein@gmail.com>
2017-01-26 16:07:50 +0100

    Add more wordcloud stuff

--- frontend/dev/jsx/routes/home.jsx
+++ frontend/dev/jsx/routes/home.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {inject, observer} from 'mobx-react'
+import EventListener from 'react-event-listener'
 
-import Wordcloud from '../components/wordcloud.jsx'
 import WordcloudControls from '../components/wordcloudControls.jsx'
 
 class Home extends Component {
@@ -9,13 +9,17 @@ class Home extends Component {
     const {getTweets, hasBeenFetched} = this.props.tweetStore
     if (!hasBeenFetched) { getTweets() }
   }
+  componentDidMount() {
+    const {initWordcloud} = this.props.tweetStore
+    initWordcloud()
+  }
   render() {
-    const {words} = this.props.tweetStore
-    console.log(words.slice())
+    const {resizeWordcloud} = this.props.tweetStore
     return (
       <div>
+        <EventListener target={window} onResize={resizeWordcloud} />
         <WordcloudControls />
-        <Wordcloud words={words.slice()} />
+        <div style={{position: 'fixed', left: 0, top: 0, right: 0, bottom: 0, zIndex: -1}} id='dataview'></div>
       </div>
     )
   }
--- frontend/dev/stores/tweetStore.js
+++ frontend/dev/stores/tweetStore.js
@@ -2,33 +2,87 @@ import {observable, action, asMap, useStrict} from 'mobx'
 // useStrict(true)
 import {get, post} from 'axios'
 import moment from 'moment'
+import * as d3 from 'd3'
+import cloud from 'd3-cloud'
 
 import getWordArray from '../lib/getWordArray'
 
 const tweetStore = observable({
   tweets: [],
-  words: [],
   hasBeenFetched: false,
   getTweetsSuccess: action((tweets) => {
     tweetStore.tweets.replace(tweets)
-    tweetStore.words.replace(getWordArray(tweets))
     tweetStore.hasBeenFetched = true
+    updateWordcloud()
   }),
-  getTweetsFailure: action(() => {
-    tweetStore.error = 'something happened..'
-  }),
-  startDate: moment().subtract(30, 'days'),
-  endDate: moment(),
+  getTweetsFailure: action(() => tweetStore.error = 'something happened..'),
+  startDate: moment().startOf('day').subtract(30, 'days'),
+  endDate: moment().startOf('day'),
   dateRangeFocusedInput: null,
-  onDateRangeFocusChange: action((newFocus) => {
-    tweetStore.dateRangeFocusedInput = newFocus
-  }),
+  onDateRangeFocusChange: action((newFocus) => tweetStore.dateRangeFocusedInput = newFocus),
   onDatesChange: action(({startDate, endDate}) => {
     tweetStore.startDate = startDate
     tweetStore.endDate = endDate
-    if (startDate && endDate) { tweetStore.getDateRange() }
-  })
+    if (startDate && endDate && moment(startDate).format() <= moment(endDate).format()) {
+      tweetStore.getDateRange()
+    }
+  }),
+  wordcloudWidth: window.innerWidth,
+  wordcloudHeight: window.innerHeight,
+  resizeWordcloud: action(() => {
+    tweetStore.wordcloudWidth = window.innerWidth
+    tweetStore.wordcloudHeight = window.innerHeight
+    tweetStore.wordcloudNode = createWordcloudNode()
+    updateWordcloud()
+  }),
+  wordcloudNode: null,
+  initWordcloud: action(() => tweetStore.wordcloudNode = createWordcloudNode())
 })
+
+// d3 functions
+const createWordcloudNode = () => {
+  document.getElementById('dataview').innerHTML = ''
+  return d3.select('#dataview').append('svg')
+              .attr('width', '100%')
+              .attr('height', '100%')
+              .append('g')
+              .attr('transform', 'translate(' + tweetStore.wordcloudWidth / 2 + ',' + tweetStore.wordcloudHeight / 2 + ')')
+}
+
+const updateWordcloud = () => {
+  const words = getWordArray(tweetStore.tweets.slice())
+  const fill = d3.scaleOrdinal(d3.schemeCategory20)
+  const layout = cloud()
+    .size([tweetStore.wordcloudWidth, tweetStore.wordcloudHeight])
+    .words(words)
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    //.rotate(0)
+    .font('Impact')
+    .fontSize(function(d) { return d.size * 10; })
+    .on('end', draw)
+  layout.start()
+
+  function draw(words) {
+    let n = tweetStore.wordcloudNode.selectAll('text').data(words)
+
+    n.enter().append('text')
+      .attr('text-anchor', 'middle')
+      .style('font-family', 'Impact')
+      .merge(n)
+      .transition()
+      .attr('transform', function(word) {
+        return 'translate(' + [word.x, word.y] + ')rotate(' + word.rotate + ')';
+      })
+      .style('font-size', function(word) {
+          return word.size + 'px'
+      })
+      .style('fill', function(word, i) { return fill(i); })
+        .text(function(word) { return word.text; })
+
+    n.exit().remove()
+  }
+}
 
 // api stuff
 tweetStore.getTweets = () => {
@@ -38,10 +92,9 @@ tweetStore.getTweets = () => {
 }
 
 tweetStore.getDateRange = () => {
-  get('http://localhost:3030/tweets/' + moment(tweetStore.startDate).format('D-M-YYYY') + '/to/' + moment(tweetStore.endDate).format('D-M-YYYY'))
+  get('http://localhost:3030/tweets/' + moment(tweetStore.startDate).format('M-D-YYYY') + '/to/' + moment(tweetStore.endDate).format('M-D-YYYY'))
     .then((resp) => tweetStore.getTweetsSuccess(resp.data))
     .catch((err) => tweetStore.getTweetsFailure())
 }
-
 
 export default tweetStore
